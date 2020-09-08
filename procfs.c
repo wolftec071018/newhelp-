b36ab325de935ab1848fabff6dd29476eef64897
Andrew <andrewdao0710@gmail.com>
2020-09-08 12:41:18 -0700

    new

--- procfs.c
+++ procfs.c
@@ -1,14 +1,31 @@
 #include "logger.h"
 #include "procfs.h"
+#include <string.h>
+#include "util.h"
 
 int pfs_hostname(char *proc_dir, char *hostname_buf, size_t buf_sz)
 {
-    return -1;
+	int fd = open_path(proc_dir,"sys/kernel/hostname");
+	if(fd<=0){
+		perror("open_path");
+		    return -1;
+}
+one_lineread(fd,hostname_buf,buf_sz);
+return 0;
 }
 
 int pfs_kernel_version(char *proc_dir, char *version_buf, size_t buf_sz)
 {
+	int fd ==open_path(proc_dir, "sys/kernel/osrelease");
+	if(fd<=0){
+	perror("open_path"); 
     return -1;
+}
+	
+	one_lineread(fd, version_buf,buf_sz);
+	stize_t strcspn(version_buf,"-");
+	version_buf[dash_pos]='\0';
+return 0;
 }
 
 int pfs_cpu_model(char *proc_dir, char *model_buf, size_t buf_sz)
