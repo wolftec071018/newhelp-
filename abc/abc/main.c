fd477195e740c6928bac0b7bf0fb64202d382cef
Andrew Dao <Andrew@Andrews-MacBook-Pro-3.local>
2020-09-08 12:43:04 -0700

    everything up to date

---
+++ abc/abc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  abc
+//
+//  Created by Andrew Dao on 9/2/20.
+//  Copyright © 2020 Andrew Dao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
